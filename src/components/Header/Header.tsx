3dc93cde00d03d734064baa30bd1f8b02c8cdae7
khumayunkh <hasanovhumayun15@gmail.com>
2023-01-11 00:12:56 +0500

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from './Header.module.sass'
+
+function Header(){
+    return(
+        <>
+        <h1 className={style.h1}>hello</h1>        
+        </>
+    )
+}
+
+export default Header
