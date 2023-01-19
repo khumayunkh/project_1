f9f0eca7b43c54ade1159eca18bedbcf169e6467
khumayunkh <hasanovhumayun15@gmail.com>
2023-01-20 00:05:58 +0500

    api

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
+import { IProducts } from "./interfaces";
 
 const client = axios.create({
     baseURL: 'https://fakestoreapi.com/products',
 });
-
 
 export const getAllProducts = async()=>{
     return await client.get('/')
@@ -19,4 +19,9 @@ export const getAllCategories = async() =>{
 
 export const getSingleCategory = async(name: string) =>{
     return await client.get(`/category/${name}`)
+}
+
+export const addNewProduct = async(data: IProducts)=>{
+    const {title, price, description, image, category} = data
+    await client.post('/', {data})
 }
