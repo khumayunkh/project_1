6acefc5c3c9dcb0792c788ae7f482c5b1318e7fc
khumayunkh <hasanovhumayun15@gmail.com>
2023-01-25 13:38:42 +0500

    login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,35 +1,31 @@
-import { Visibility, VisibilityOff } from "@mui/icons-material"
-import { FormControl, IconButton, Input, InputAdornment, InputLabel, TextField } from "@mui/material"
-import React, { useEffect } from "react"
+import React from "react"
 import { useForm } from "react-hook-form"
-import { NavLink } from "react-router-dom"
+import { Navigate, NavLink } from "react-router-dom"
 import { ILogin } from "../../api/auth/interfaces"
-import { useAppDispatch } from "../../hooks"
+import { useAppDispatch, useAppSelector } from "../../hooks"
 import { loginThunk } from "../../reducers/auth/authReducer"
 import style from './Login.module.sass'
 
 
 function Login(){
     const dispatch = useAppDispatch()
+    const isAuth = useAppSelector(state => state.auth.userIsAuth)
     const {register, handleSubmit} = useForm<ILogin>()
-    const [showPassword, setShowPassword] = React.useState(false);
-
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-  
-    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-      event.preventDefault();
-    };
 
     const username = register('username', {required: true})
     const password = register('password', {required: true})
-    
+
     const onSubmit = (data: ILogin)  => {
         dispatch(loginThunk({
             username: data.username,
             password: data.password
         }))
     }
-    
+
+    if(isAuth){
+      return <Navigate to='/'/>
+    }
+
     return(
         <>
         <div className={style.container}>
@@ -38,31 +34,8 @@ function Login(){
                 <form
                     onSubmit={handleSubmit(onSubmit)} 
                     className={style.form}>
-                    <TextField 
-                        {...username}
-                        id="standard-basic" 
-                        label="Standard" 
-                        variant="standard"
-                    />
-                    <FormControl variant="standard">
-                        <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-                        <Input
-                          id="standard-adornment-password"
-                          type={showPassword ? 'text' : 'password'}
-                          endAdornment={
-                            <InputAdornment position="end">
-                              <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={handleClickShowPassword}
-                                onMouseDown={handleMouseDownPassword}
-                                {...password}
-                              >
-                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                              </IconButton>
-                            </InputAdornment>
-                          }
-                        />
-                    </FormControl>
+                    <input {...username} placeholder='Email'/>
+                    <input {...password} placeholder='Password' type='password'/>
                     <button className={style.btn}>Login</button>
                     <NavLink className={style.navigate_btn} to='/signup'>SignUp</NavLink>
                 </form>
